e37389348ab49aac5751ac06bce128e928d75266
emanuel123321 <costandachi89@gmail.com>
2023-06-11 23:45:57 +0300

    I recreated the logic and finished the project

--- script.rb
+++ script.rb
@@ -3,13 +3,30 @@ def caesar_cipher(text, key)
     char_array = text.split("")
     
     char_array.each do |character|
-        keycode = character.ord + key
-        if keycode > 122
-            keycode = keycode - 26
+        keycode = character.ord
+
+        if keycode.between?(96, 122)
+            keycode += key
+            if keycode < 123
+                new_array << keycode.chr
+            elsif keycode > 122
+                keycode -= 26
+                new_array << keycode.chr
+            end
+        elsif keycode.between?(65, 90)
+            keycode += key
+            if keycode < 91
+                new_array << keycode.chr
+            elsif keycode > 90
+                keycode -= 26
+                new_array << keycode.chr
+            end 
+        else
+            new_array<< keycode.chr
         end
-        new_array << keycode.chr
+        
     end
     new_array.join("")
 end
 
-p caesar_cipher("text", 3)
+p caesar_cipher("Ebiil jv kxjb fp BJXKRBI ! Kf`b ql jbbq vlr", 3)
