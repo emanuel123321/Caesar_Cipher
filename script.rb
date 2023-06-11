252eca6e440a387a0a0aa3006885abc76861a4c6
emanuel123321 <costandachi89@gmail.com>
2023-06-11 21:13:51 +0300

    fix the bug for the last letters

--- script.rb
+++ script.rb
@@ -1,11 +1,15 @@
-def caesar_cipher(text, key, new_array = [])
+def caesar_cipher(text, key)
+    new_array = []
     char_array = text.split("")
     
     char_array.each do |character|
         keycode = character.ord + key
+        if keycode > 122
+            keycode = keycode - 26
+        end
         new_array << keycode.chr
     end
     new_array.join("")
 end
 
-p caesar_cipher("hello", 3)
+p caesar_cipher("text", 3)
